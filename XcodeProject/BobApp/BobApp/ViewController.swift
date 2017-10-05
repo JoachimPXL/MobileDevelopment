11d6abb250013fdacdac663965e67b8bc59edb56
JoachimPXL <joachim.zeelmaekers@student.pxl.be>
2017-10-05 20:27:23 +0200

    Initiële commit met leeg Xcode project om in te werken

---
+++ XcodeProject/BobApp/BobApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BobApp
+//
+//  Created by Joachim Zeelmaekers on 05/10/2017.
+//  Copyright © 2017 Joachim Zeelmaekers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
