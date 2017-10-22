110724fd982edd5d6541a66105459dd0baad79e5
JoachimPXL <joachim.zeelmaekers@student.pxl.be>
2017-10-22 16:58:43 +0200

    Locatievoorzieningrequest + opvragen van gps coördinaten done en getest op mobiel toestel.

--- XcodeProject/BobApp/BobApp/Controllers/PulsatorViewController.swift
+++ XcodeProject/BobApp/BobApp/Controllers/PulsatorViewController.swift
@@ -8,28 +8,46 @@
 
 import Foundation
 import Pulsator
+import CoreLocation
 
-class PulsatorViewController: UIViewController {
+class PulsatorViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
     
+    @IBAction func scanRadius(_ sender: Any) {
+        //get current location
+        getLongitudeAndLatitude()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view, typically from a nib.
         
+        //When someone taps in the app while typing (not in keyboard), the keyboard gets cancelled
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+        
+        //self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
     
     override func viewDidAppear(_ animated:Bool) {
         super.viewDidAppear(false)
-        let pulsator = Pulsator()
-        pulsator.position = CGPoint(x: 200, y: 200)
-        pulsator.numPulse = 2
-        pulsator.radius = 240
-        pulsator.backgroundColor = UIColor(red:1, green:0, blue: 0, alpha:1).cgColor
-        pulsator.animationDuration = 3
-        pulsator.pulseInterval = 1
-        
-        view.layer.addSublayer(pulsator)
-        pulsator.start()
+            let pulsator = Pulsator()
+            pulsator.position = CGPoint(x: 200, y: 200)
+            pulsator.numPulse = 2
+            pulsator.radius = 240
+            pulsator.backgroundColor = UIColor(red:1, green:0, blue: 0, alpha:1).cgColor
+            pulsator.animationDuration = 3
+            pulsator.pulseInterval = 1
+            
+            view.layer.addSublayer(pulsator)
+            pulsator.start()
     }
     
     override func didReceiveMemoryWarning() {
@@ -37,5 +55,23 @@ class PulsatorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func getLongitudeAndLatitude() {
+        let locValue = locationManager.location?.coordinate
+        
+        print("locations = \(locValue?.latitude) \(locValue?.longitude)")
+    }
+    
+    @objc
+    func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
+    }
     
 }
