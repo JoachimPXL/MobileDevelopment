90154a0430140039e3a772cc7fa4cb475682fb70
JoachimPXL <joachim.zeelmaekers@student.pxl.be>
2017-11-06 19:29:57 +0100

    remake van de api call naar events, nog in progress

--- XcodeProject/BobApp/BobApp/Controllers/PulsatorViewController.swift
+++ XcodeProject/BobApp/BobApp/Controllers/PulsatorViewController.swift
@@ -23,75 +23,6 @@ class PulsatorViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var radiusSlider: UISlider!
     var mappedEvents: [Event] = []
     
-    @IBAction func radiusValueChanged(_ sender: Any) {
-        radiusSlider.setValue(Float(lroundf(radiusSlider.value)), animated: true)
-        radiusTextField.text = "\(radiusSlider.value) KM"
-    }
-
-    @IBAction func scanRadius(_ sender: Any) {
-        
-        
-        
-    }
-    
-    enum MyError: Error{
-        case ApiCallFailedError
-    }
-    
-    func getEventsFromApi() throws -> MyError {
-        let longitude:Double! = getLongitude()
-        let latitude:Double! = getLatitude()
-        let radiusInMeters: Int! = Int(radiusSlider.value * 1000)
-        let time:String! = afternoonOrEvening.titleForSegment(at: afternoonOrEvening.selectedSegmentIndex)
-        let accessToken:String! = keychain.get("accessToken")!
-        
-        if(longitude != nil && latitude != nil && radiusInMeters != 0 && time != nil && accessToken != nil) {
-            //  let url = "http://0.0.0.0:3000/events?lat=\(latitude)lng=\(longitude)&distance=\(radiusInMeters)&sort=venue&accesToken=\(accessToken)"
-            if let longitude = longitude, let latitude = latitude ,let radiusInMeters = radiusInMeters , let time = time , let accessToken = accessToken {
-                var url = "http://0.0.0.0:3000/events?&lat=\(latitude)&lng=\(longitude)&distance=\(radiusInMeters)&sort=venue&accessToken=\(accessToken)"
-                Alamofire.request(url).validate().responseJSON { response in
-                    debugPrint(response)
-                    if let jsonObj = response.result.value {
-                        let json = JSON(jsonObj)
-                        for (key, event) in json["events"] {
-                            //                          get coordinates from event and your current to calculate distance.
-                            let coordinate₀ = CLLocation(latitude: event["place"]["location"]["latitude"].double!, longitude: event["place"]["location"]["longitude"].double!)
-                            let coordinate₁ = CLLocation(latitude: latitude, longitude: longitude)
-                            //                          distance result in meters
-                            let distanceInMeters = coordinate₀.distance(from: coordinate₁)
-                            //                          properties from event only those who we need
-                            let title = event["name"].string
-                            let attending = event["stats"]["attending"].int
-                            let startdate = event["startTime"].string
-                            let enddate = event["endTime"].string
-                            let orgianisator = event["vanue"]["name"].string
-                            
-                            let e = Event(name: title!, attending: attending!,afstand: distanceInMeters)
-                            self.mappedEvents.append(e)
-                        }
-                        
-                        print("array length: ")
-                        print(self.mappedEvents.count)
-                    }
-                }
-            }
-            
-        } else {
-            throw MyError.ApiCallFailedError
-        }
-        return MyError.ApiCallFailedError
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if (segue.identifier == "ScanEventsSegue") {
-            if let eventTableViewController = segue.destination as? EventTableViewController {
-                print("tapped")
-                self.mappedEvents.append(Event(name: "Testevent", attending: 90, afstand: 90.10))
-                eventTableViewController.mappedEvents2 = mappedEvents
-            }
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -128,6 +59,39 @@ class PulsatorViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func radiusValueChanged(_ sender: Any) {
+        radiusSlider.setValue(Float(lroundf(radiusSlider.value)), animated: true)
+        radiusTextField.text = "\(radiusSlider.value) KM"
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "ScanEventsSegue") {
+            if let eventTableViewController = segue.destination as? EventTableViewController {
+                print("tapped")
+                //nodige data voor api call meesturen met segue naar volgende controller.
+                eventTableViewController.accessToken = keychain.get("accessToken")!
+                eventTableViewController.latitude = getLatitude()
+                eventTableViewController.longitude = getLongitude()
+                eventTableViewController.radiusInMeters = Int(radiusSlider.value * 1000)
+                eventTableViewController.time = afternoonOrEvening.titleForSegment(at: afternoonOrEvening.selectedSegmentIndex)
+            }
+        }
+    }
+
+    @IBAction func scanRadius(_ sender: Any) {
+        let longitude:Double! = getLongitude()
+        let latitude:Double! = getLatitude()
+        let radiusInMeters: Int! = Int(radiusSlider.value * 1000)
+        let time:String! = afternoonOrEvening.titleForSegment(at: afternoonOrEvening.selectedSegmentIndex)
+        let accessToken:String! = keychain.get("accessToken")!
+        
+        if(longitude != nil && latitude != nil && radiusInMeters != 0 && time != nil && accessToken != nil) {
+        self.performSegue(withIdentifier: "ScanEventsSegue", sender: nil)
+        } else {
+            print("Alert")
+        }
+        
+    }
     
     func getLongitude() -> Double! {
         let locValue = locationManager.location?.coordinate
--- XcodeProject/BobApp/BobApp/Controllers/TableControllers/EventTableViewController.swift
+++ XcodeProject/BobApp/BobApp/Controllers/TableControllers/EventTableViewController.swift
@@ -7,27 +7,37 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import Alamofire
+import PromiseKit
+import SwiftyJSON
+import CoreLocation
 
 class EventTableViewController: UITableViewController {
-    var mappedEvents2: [Event] = []
+    var mappedEvents: [Event] = []
+    var longitude:Double! = 0
+    var latitude:Double! = 0
+    var radiusInMeters: Int! = 0
+    var time:String! = ""
+    var accessToken:String! = FBSDKAccessToken.current().tokenString
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getEventsFromApi()
         tableView.reloadData()
-       
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(false)
-        if(mappedEvents2.count == 1) {
+        if(mappedEvents.count == 1) {
             let alertController = UIAlertController(title: "Evenementen", message:
-                "Er is \(mappedEvents2.count) evenement gevonden.", preferredStyle: UIAlertControllerStyle.alert)
+                "Er is \(mappedEvents.count) evenement gevonden.", preferredStyle: UIAlertControllerStyle.alert)
             alertController.addAction(UIAlertAction(title: "Ga door", style: UIAlertActionStyle.destructive,handler: nil))
             
             self.present(alertController, animated: true, completion: nil)
         } else {
             let alertController = UIAlertController(title: "Evenementen", message:
-                "Er zijn \(mappedEvents2.count) evenementen gevonden.", preferredStyle: UIAlertControllerStyle.alert)
+                "Er zijn \(mappedEvents.count) evenementen gevonden.", preferredStyle: UIAlertControllerStyle.alert)
             alertController.addAction(UIAlertAction(title: "Ga door", style: UIAlertActionStyle.destructive,handler: nil))
             
             self.present(alertController, animated: true, completion: nil)
@@ -39,7 +49,7 @@ class EventTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mappedEvents2.count
+        return mappedEvents.count
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -55,7 +65,7 @@ class EventTableViewController: UITableViewController {
         }
         
         // Fetches the appropriate meal for the data source layout.
-        let event = mappedEvents2[indexPath.row]
+        let event = mappedEvents[indexPath.row]
         //VehiclesDatabase.instance.deleteVehicle(vid: 1)
         cell.eventNam.text = "\(event.name)"
         
@@ -79,5 +89,50 @@ class EventTableViewController: UITableViewController {
         }
     }
     
+    
+    enum MyError: Error{
+        case ApiCallFailedError
+    }
+    
+    func getEventsFromApi() {
+        //  let url = "http://0.0.0.0:3000/events?lat=\(latitude)lng=\(longitude)&distance=\(radiusInMeters)&sort=venue&accesToken=\(accessToken)"
+        if let longitude = longitude, let latitude = latitude ,let radiusInMeters = radiusInMeters , let time = time , let accessToken = accessToken {
+            var url = "http://0.0.0.0:3000/events?&lat=\(latitude)&lng=\(longitude)&distance=\(radiusInMeters)&sort=venue&accessToken=\(accessToken)"
+            Alamofire.request(url).validate().responseJSON { response in
+                debugPrint(response)
+                if let jsonObj = response.result.value {
+                    let json = JSON(jsonObj)
+                    for (key, event) in json["events"] {
+                        //                          get coordinates from event and your current to calculate distance.
+                        let coordinate₀ = CLLocation(latitude: event["place"]["location"]["latitude"].double!, longitude: event["place"]["location"]["longitude"].double!)
+                        let coordinate₁ = CLLocation(latitude: latitude, longitude: longitude)
+                        //                          distance result in meters
+                        let distanceInMeters = coordinate₀.distance(from: coordinate₁)
+                        //                          properties from event only those who we need
+                        let title = event["name"].string
+                        let attending = event["stats"]["attending"].int
+                        let startdate = event["startTime"].string
+                        let enddate = event["endTime"].string
+                        let orgianisator = event["vanue"]["name"].string
+                        
+                        let e = Event(name: title!, attending: attending!,afstand: distanceInMeters)
+                        self.mappedEvents.append(e)
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let _:CLLocationCoordinate2D = manager.location!.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
+    }
 }
+
+
+
 
